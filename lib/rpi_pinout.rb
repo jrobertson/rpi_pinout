6c0eea3c86b41d8d41e2fecd0c125f13f6b661ed
Your Name <you@example.com>
2017-05-10 19:44:31 +0100

    Now using the PinX gem for on, off, and blink methods.

--- lib/rpi_pinout.rb
+++ lib/rpi_pinout.rb
@@ -2,15 +2,14 @@
 
 # file: rpi_pinout.rb
 
-
-HIGH = 1
-LOW = 0
+require 'pinx'
 
 
-class RPiPinOut
+class RPiPinOut < PinX
   
   def initialize(id)
-
+    
+    super(id)
     @id = id    
     unexport()
     
@@ -21,62 +20,16 @@ class RPiPinOut
     
   end
 
-  def on(duration=nil)
-
-    set_pin HIGH; 
-    @state = :on
-    (sleep duration; self.off) if duration
-  end
-
-  def off(duration=nil)
-
-    return if self.off?
-    set_pin LOW      
-    @state = :off
-    (sleep duration; self.on) if duration
-  end
   
-  alias high on # opposite of low
-  alias open on # opposite of close
-  alias lock on # opposite of unlock
-  
-  alias stop off        
-  alias low off
-  alias close off
-  alias unlock off
+  protected
 
-  def blink(seconds=0.5, duration: nil)
-
-    @state = :blink
-    t2 = Time.now + duration if duration
-
-    Thread.new do
-      while @state == :blink do
-        (set_pin HIGH; sleep seconds; set_pin LOW; sleep seconds) 
-        self.off if duration and Time.now >= t2
-      end
-      
-    end
-  end
-  
-  alias oscillate blink
-
-  def on?()  @state == :on  end
-  def off?() @state == :off end
-
-  # set val with 0 (off) or 1 (on)
-  #
   def set_pin(val)
 
-    state = @state
-    File.write "/sys/class/gpio/gpio#{@id}/value", val
-    @state = state
+    super(val)
+    File.write "/sys/class/gpio/gpio#{@id}/value", (val ? 1 : 0)
     
   end
   
-  def to_s()
-    @id
-  end
   
   private
   
@@ -89,4 +42,5 @@ class RPiPinOut
 
     File.write "/sys/class/gpio/unexport", @id
   end
+  
 end
